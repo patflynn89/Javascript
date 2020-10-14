27cf687a53c8f3425824f7191628588fcdc133bd
patflynn89 <xbl_bachhuber@hotmail.com>
2020-10-14 12:18:42 +0200

    Styling done

--- Games/Calculator/js/calculator.js
+++ Games/Calculator/js/calculator.js
@@ -1,12 +1,8 @@
 $(document).ready(function(){
-    alert("griaßde");
     function result(first) {
         return (first+3);
     }
-    document.getElementById("ergebnis").innerHTML = result(1);
-    $("p").click(function () { 
-        $(this).hide();
-    });
+  
     
 });
 
